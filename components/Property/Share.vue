a8152cc2d2439cfbaff49ab5e22d12a3be96862c
Neil Infante <neil.infante@eflexervices.com>
2024-04-25 14:31:24 +0800

    Put share section into its own component

---
+++ components/Property/Share.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="w-full max-w-6xl p-0">
+    <div class="flex items-center justify-center gap-2 text-lg md:justify-end">
+      <Button
+        size="small"
+        class="hover:text-pink-500">
+        <i class="pi pi-heart-fill"></i>
+      </Button>
+      <Button
+        size="small"
+        class="hover:text-yellow-500"
+        ><i class="pi pi-share-alt"></i
+      ></Button>
+      <Button
+        size="small"
+        class="hover:text-yellow-500"
+        ><i class="pi pi-envelope"></i
+      ></Button>
+      <Button
+        size="small"
+        class="hover:text-red-500"
+        ><i class="pi pi-flag-fill"></i
+      ></Button>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+</script>
--- pages/property/[name]/index.vue
+++ pages/property/[name]/index.vue
@@ -20,31 +20,7 @@
     </Galleria>
 
     <!-- Share -->
-    <section class="w-full max-w-6xl p-0">
-      <div
-        class="flex items-center justify-center gap-2 text-lg md:justify-end">
-        <Button
-          size="small"
-          class="hover:text-pink-500">
-          <i class="pi pi-heart-fill"></i>
-        </Button>
-        <Button
-          size="small"
-          class="hover:text-yellow-500"
-          ><i class="pi pi-share-alt"></i
-        ></Button>
-        <Button
-          size="small"
-          class="hover:text-yellow-500"
-          ><i class="pi pi-envelope"></i
-        ></Button>
-        <Button
-          size="small"
-          class="hover:text-red-500"
-          ><i class="pi pi-flag-fill"></i
-        ></Button>
-      </div>
-    </section>
+    <PropertyShare />
 
     <section
       class="flex flex-col justify-between w-full max-w-6xl p-4 text-gray-700 bg-white rounded-md md:flex-row">
