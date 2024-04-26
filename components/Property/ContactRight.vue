0f3cf28c3a854a04bbcf1cbc607a2f0e86ac59b6
Neil Infante <neil.infante@eflexervices.com>
2024-04-26 15:23:10 +0800

    Add contact on the right of page, details section and features sections

---
+++ components/Property/ContactRight.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col w-full gap-2 bg-white rounded-md p-8 lg:max-w-[35%]">
+    <h2 class="text-2xl font-bold text-slate-700">Contact Broker</h2>
+
+    <div class="flex-1 space-y-4">
+      <div class="flex flex-col">
+        <div class="text-xl font-bold text-slate-700">Name:</div>
+        <div class="font-semibold md:text-xl text-slate-500">Neil Infante</div>
+      </div>
+
+      <div>
+        <div class="text-xl font-bold text-slate-700">Phone:</div>
+        <div class="font-semibold md:text-xl text-slate-500">(63) 121-7141</div>
+      </div>
+
+      <div>
+        <div class="text-xl font-bold text-slate-700">Email:</div>
+        <div class="font-semibold sm:text-xl text-slate-500"
+          >neil.infante@eflexervices.com</div
+        >
+      </div>
+
+      <div>
+        <div class="pb-4 text-xl font-bold text-slate-700">Message:</div>
+        <FormKit type="textarea" />
+      </div>
+      <Button class="w-full">Send Message</Button>
+    </div>
+  </div>
+</template>
