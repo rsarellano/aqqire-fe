40063f43689aa7a503450f7fa661bf26a6eb4819
Neil Infante <neil.infante@eflexervices.com>
2024-04-26 15:22:43 +0800

    Fix card on mobile xs screens

--- components/Property/CardSmall.vue
+++ components/Property/CardSmall.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="relative flex flex-col justify-end w-full h-full overflow-hidden border rounded group hover:shadow-xl md:min-w-64 md:min-h-52 lg:min-h-32 min-h-52 min-w-52 lg:min-w-52">
+    class="relative flex flex-col justify-end flex-1 overflow-hidden border rounded group hover:shadow-xl min-w-56 w-full max-w-56 h-full">
     <img
       src="https://lion-coders.com/demo/html/sarchholm-real-estate-template/images/property/property_1.jpg"
       class="scale-125 h-full duration-700 ease-in-out group-hover:scale-[1.8] -z-9999 absolute inset-0 w-full" />
 
     <div
-      class="z-50 flex flex-col w-full gap-2 overflow-hidden duration-700 bg-gray-700/30">
+      class="z-20 flex flex-col w-full gap-2 overflow-hidden duration-700 bg-gray-700/30">
       <div class="p-2 space-y-2">
         <div class="flex gap-[2px] text-white">
           <Tag
@@ -24,7 +24,7 @@
     <div class="absolute top-2 right-2">
       <Tag
         value="For Sale"
-        class="bg-blue-400"
+        class="z-30 bg-blue-400"
         rounded />
     </div>
   </div>
