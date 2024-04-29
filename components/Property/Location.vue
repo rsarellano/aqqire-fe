4768fc842f2bb05e5861c8ee17bdf6f80bc3f94d
Neil Infante <neil.infante@eflexervices.com>
2024-04-29 15:30:33 +0800

    Separate location into its own component

---
+++ components/Property/Location.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="flex flex-col w-full max-w-6xl gap-4 mt-2 lg:flex-row">
+    <div class="p-8 space-y-2 bg-white rounded-md grow">
+      <h2 class="text-2xl font-bold uppercase">Location</h2>
+      <img
+        src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/191:100/w_1280,c_limit/GoogleMapTA.jpg" />
+    </div>
+  </section>
+</template>
+
