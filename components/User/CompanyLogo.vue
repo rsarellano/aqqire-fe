3a6cf1891e73ecf1dea1eb70d8088c77e393b532
Neil Infante <neil.infante@eflexervices.com>
2024-05-09 21:16:41 +0800

    Add spaceing to upload buttons

--- components/User/CompanyLogo.vue
+++ components/User/CompanyLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-4 text-slate-600">
+  <div class="flex flex-col gap-4 pb-4 text-slate-600">
     <h1 class="pb-4 text-2xl font-semibold border-b border-slate-200">
       Upload Company Logo
     </h1>
