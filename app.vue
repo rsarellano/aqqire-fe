9ee71b616db71c23ddc9115d79b5cf8bbaf37454
aqqire1-admin <calixto.ong@eflexervices.com>
2024-05-21 22:48:03 +0800

    Add user auth function to app.vue

--- app.vue
+++ app.vue
@@ -4,7 +4,12 @@
   </NuxtLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//added authentication to access user specific functions 
+import { useAuth } from '#imports'
+const { signIn, token, data, status, lastRefreshedAt, signOut, getSession } = useAuth()
+
+</script> 
 
 <style>
   @import url("~/assets/css/base.css");
