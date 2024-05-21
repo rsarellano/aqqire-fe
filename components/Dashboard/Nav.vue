7dda61f23d399226fd408c21fd917cea861c9a44
Neil Infante <neil.infante@eflexervices.com>
2024-05-21 21:43:42 +0800

    Add Dashboard Properties Listing

---
+++ components/Dashboard/Nav.vue
@@ -0,0 +1,47 @@
+<template>
+  <Menubar
+    :model="routes"
+    class="my-2"
+  >
+    <template #item="{ item, props }">
+      <NuxtLink
+        :to="item.route"
+        class="w-full mr-6"
+      >
+        <span
+          v-bind="props.label"
+          class="capitalize text-slate-500"
+          :class="{ 'font-bold !text-blue-500': route.path === item.route }"
+        >
+          {{ item.label }}
+        </span>
+      </NuxtLink>
+    </template>
+  </Menubar>
+</template>
+
+<script setup lang="ts">
+  const route = useRoute()
+  const routes = ref([
+    {
+      label: "active properties",
+      route: "/dashboard/property/active",
+    },
+    {
+      label: "inactive properties",
+      route: "/dashboard/property/inactive",
+    },
+    {
+      label: "saved properties",
+      route: "/dashboard/property/saved",
+    },
+    {
+      label: "sold properties",
+      route: "/dashboard/property/sold",
+    },
+    {
+      label: "add property",
+      route: "/dashboard/property/add",
+    },
+  ])
+</script>
