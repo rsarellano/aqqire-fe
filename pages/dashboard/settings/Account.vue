82303119042d8a1af7e67b9fc3ca0473cf860e5f
Neil Infante <neil.infante@eflexervices.com>
2024-10-07 20:15:19 +0800

    Add Basic info and password

--- pages/dashboard/settings/Account.vue
+++ pages/dashboard/settings/Account.vue
@@ -1,34 +1,55 @@
 <template>
   <div class="h-full md:p-6">
     <div
-      class="h-full p-4 space-y-5 text-gray-700 rounded md:px-7 lg:p-6 lg:px-12 bg-slate-100"
-    >
+      class="h-full p-4 space-y-5 text-gray-700 rounded md:px-7 lg:p-6 lg:px-12 bg-slate-100">
       <h1 class="text-3xl font-bold">Account Settings</h1>
-      <Avatar :image="image" size="avatar" shape="circle" />
+      <Avatar
+        :image="image"
+        size="avatar"
+        shape="circle" />
       <FileUpload
         mode="basic"
         accept="image/*"
         chooseLabel="Change Avatar"
         auto
         customUpload
-        @uploader="changeImage"
-      />
+        @uploader="changeImage" />
 
-      <FormKit label="First Name" outer-class="max-w-sm" />
-      <FormKit label="Last Name" outer-class="max-w-sm" />
+      <FormKit
+        label="First Name"
+        v-model="user.first_name"
+        outer-class="max-w-sm" />
+
+      <FormKit
+        label="Last Name"
+        v-model="user.last_name"
+        outer-class="max-w-sm" />
 
       <!-- Email -->
       <FormKit
         label="Email Address"
+        v-model="user.email"
         outer-class="max-w-sm"
-        :disabled="emailDisabled"
-      />
+        :disabled="emailDisabled" />
       <Button
         class="w-full max-w-sm lg:w-max"
-        @click="emailDisabled = !emailDisabled"
-      >
+        @click="emailDisabled = !emailDisabled">
         Change Email
       </Button>
+
+      <div class="flex flex-col justify-end gap-2 p-4 border-b-2 lg:flex-row">
+        <Button
+          severity="secondary"
+          class="font-semibold"
+          @click="refresh">
+          Cancel
+        </Button>
+        <Button
+          class="font-semibold"
+          @click="updateBasicInfo">
+          Save Changes
+        </Button>
+      </div>
 
       <!-- Password -->
       <h2 class="text-2xl font-bold">Password</h2>
@@ -40,14 +61,46 @@
       <ToggleButton
         v-model="changePassword"
         onLabel="Close Password"
-        offLabel="Change Password"
-      />
+        offLabel="Change Password" />
 
       <template v-if="changePassword">
         <div class="max-w-sm">
-          <FormKit label="Current Password" />
-          <FormKit label="New Password" />
-          <FormKit label="New Password Confirmation" />
+          <FormKit
+            type="password"
+            label="Current Password"
+            v-model="password.current"
+            suffix-icon="eyeClosed"
+            @suffix-icon-click="handleIconClick"
+            suffix-icon-class="hover:text-blue-500" />
+          <FormKit
+            type="password"
+            label="New Password"
+            v-model="password.newPassword"
+            suffix-icon="eyeClosed"
+            @suffix-icon-click="handleIconClick"
+            suffix-icon-class="hover:text-blue-500" />
+          <FormKit
+            type="password"
+            label="New Password Confirmation"
+            v-model="password.newPassword2"
+            suffix-icon="eyeClosed"
+            @suffix-icon-click="handleIconClick"
+            suffix-icon-class="hover:text-blue-500" />
+
+          <div
+            class="flex flex-col justify-end gap-2 p-4 border-b-2 lg:flex-row">
+            <Button
+              severity="secondary"
+              class="font-semibold"
+              @click="refresh">
+              Cancel
+            </Button>
+            <Button
+              class="font-semibold"
+              @click="updatePassword">
+              Save Changes
+            </Button>
+          </div>
         </div>
       </template>
 
@@ -59,26 +112,68 @@
       </p>
       <InputSwitch v-model="smartSync" />
       -->
-
-      <div class="flex flex-col justify-end gap-2 p-4 border-t-2 lg:flex-row">
-        <Button severity="secondary" class="font-semibold">Cancel</Button>
-        <Button class="font-semibold">Save Changes</Button>
-      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { FileUploadUploaderEvent } from "primevue/fileupload";
-type FileUrl = File & { objectURL: string };
+  import type { FileUploadUploaderEvent } from "primevue/fileupload"
+  import { useAuth } from "#imports"
 
-const image = ref("https://picsum.photos/id/237/200/300");
-const emailDisabled = ref(true);
-const changePassword = ref(false);
-const smartSync = ref(false);
+  type FileUrl = File & { objectURL: string }
+  const { data } = useAuth()
+  const router = useRouter()
+  const emailDisabled = ref(true)
+  const changePassword = ref(false)
+  const smartSync = ref(false)
+  const image = ref(data.value?.profile_pic)
 
-const changeImage = (event: FileUploadUploaderEvent) => {
-  const target = event.files as FileUrl[];
-  image.value = target[0].objectURL;
-};
+  const user = ref({
+    first_name: data.value.first_name,
+    last_name: data.value.last_name,
+    email: data.value.email,
+  })
+
+  const password = ref({
+    current: "",
+    newPassword: "",
+    newPassword2: "",
+  })
+
+  // TODO connect to backend
+  const updateBasicInfo = () => {
+    const userInfo = {
+      ...data.value,
+      ...user.value,
+    }
+    console.log(userInfo, "usernfo")
+  }
+
+  const handleIconClick = (node, e) => {
+    node.props.suffixIcon =
+      node.props.suffixIcon === "eye" ? "eyeClosed" : "eye"
+    node.props.type = node.props.type === "password" ? "text" : "password"
+  }
+
+  const updatePassword = () => {
+    if (password.value.newPassword !== password.value.newPassword2) {
+      console.log("passwords are not the same")
+      return
+    }
+    if (password.value.current === "") {
+      console.log("enter your current password")
+      return
+    }
+
+    console.log(password.value, "usernfo")
+  }
+
+  const refresh = async () => {
+    router.go(0)
+  }
+
+  const changeImage = (event: FileUploadUploaderEvent) => {
+    const target = event.files as FileUrl[]
+    image.value = target[0].objectURL
+  }
 </script>
--- pages/login.vue
+++ pages/login.vue
@@ -18,12 +18,21 @@
         name="email"
         type="text"
         validation="email" />
+
       <FormKit
         v-model="password"
         label="Password"
         type="password"
         validation="required"
         required />
+
+      <FormKit
+        type="checkbox"
+        label="Remember Me"
+        name="rememberMe"
+        v-model="rememberMe"
+        decoratorIcon="check" />
+
       <Button type="submit">Sign In</Button>
       <NuxtLink
         to="/register"
@@ -35,54 +44,55 @@
 </template>
 
 <script setup lang="ts">
-//added authentication to access user specific functions
+  //added authentication to access user specific functions
   import { useAuth } from "#imports"
   const { signIn, status } = useAuth()
 
   const username = ref("")
   const password = ref("")
+  const rememberMe = ref(false)
   const checkFields = ref(false)
   const checkMessage = ref()
 
-definePageMeta({
-  layout: "auth",
-  //add authentication specifics
-  auth: {
-    unauthenticatedOnly: true,
-    navigateAuthenticatedTo: "/dashboard",
-  },
+  definePageMeta({
+    layout: "auth",
+    //add authentication specifics
+    auth: {
+      unauthenticatedOnly: true,
+      navigateAuthenticatedTo: "/dashboard",
+    },
   })
 
-//handle user login and rediret to dashboard
-const handleSignIn = async () => {
-  try {
-    if (username.value === "" || password.value === "") {
+  //handle user login and rediret to dashboard
+  const handleSignIn = async () => {
+    try {
+      if (username.value === "" || password.value === "") {
         throw new Error("Invalid Login Credentials")
-    }
+      }
       checkFields.value = false
 
-    const userLogin = await signIn(
-      {
-        username: username.value,
-        password: password.value,
-      },
-      {
-        callbackUrl: "/dashboard",
+      const userLogin = await signIn(
+        {
+          username: username.value,
+          password: password.value,
+        },
+        {
+          callbackUrl: "/dashboard",
         }
       )
       console.log(userLogin)
-  } catch (error) {
-    checkFields.value = true;
-    checkMessage.value = "Invalid Credentials";
+    } catch (error) {
+      checkFields.value = true
+      checkMessage.value = "Invalid Credentials"
+    }
   }
-};
 
-onMounted(async () => {
-  const { status } = useAuth();
-  if (status.value === "authenticated") {
-    reloadNuxtApp({ path: "/dashboard" });
-  } else {
-    reloadNuxtApp({ path: "/login" });
-  }
-});
+  onMounted(async () => {
+    const { status } = useAuth()
+    if (status.value === "authenticated") {
+      reloadNuxtApp({ path: "/dashboard" })
+    } else {
+      reloadNuxtApp({ path: "/login" })
+    }
+  })
 </script>
