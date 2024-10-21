beb6ffb94b8d51151201806c9860cace354c1ff1
Neil Infante <neil.infante@eflexervices.com>
2024-10-21 16:32:11 +0800

    Add random images to single property page

--- pages/property/[id]/index.vue
+++ pages/property/[id]/index.vue
@@ -96,9 +96,10 @@
 </template>
 
 <script setup lang="ts">
+  import { getRandomImage, images as RandomImages } from "~/pages/data"
   import { isPremium } from "~/store/isPremium"
-  import { useRuntimeConfig } from "#app";
-  const apiUrl = useRuntimeConfig().public.API_BASE_URL;
+  import { useRuntimeConfig } from "#app"
+  const apiUrl = useRuntimeConfig().public.API_BASE_URL
 
   type Property = {
     id: number
@@ -236,128 +237,38 @@
 
   const route = useRoute()
   const { data } = await useFetch<Property>(
-    `${apiUrl}/property/view/${route.params.id}`
+    `${apiUrl}/property/${route.params.id}`
   )
   const images = [
     {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria1.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria1s.jpg",
+      itemImageSrc: RandomImages[0],
+      thumbnailImageSrc: RandomImages[0],
       alt: "Description for Image 1",
       title: "Title 1",
     },
     {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria2.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria2s.jpg",
+      itemImageSrc: RandomImages[1],
+      thumbnailImageSrc: RandomImages[1],
       alt: "Description for Image 2",
       title: "Title 2",
     },
     {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria3.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria3s.jpg",
+      itemImageSrc: RandomImages[2],
+      thumbnailImageSrc: RandomImages[2],
       alt: "Description for Image 3",
       title: "Title 3",
     },
     {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria4.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria4s.jpg",
+      itemImageSrc: RandomImages[3],
+      thumbnailImageSrc: RandomImages[3],
       alt: "Description for Image 4",
       title: "Title 4",
     },
     {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria5.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria5s.jpg",
+      itemImageSrc: RandomImages[4],
+      thumbnailImageSrc: RandomImages[4],
       alt: "Description for Image 5",
       title: "Title 5",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria6.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria6s.jpg",
-      alt: "Description for Image 6",
-      title: "Title 6",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria7.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria7s.jpg",
-      alt: "Description for Image 7",
-      title: "Title 7",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria8.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria8s.jpg",
-      alt: "Description for Image 8",
-      title: "Title 8",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria9.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria9s.jpg",
-      alt: "Description for Image 9",
-      title: "Title 9",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria10s.jpg",
-      alt: "Description for Image 10",
-      title: "Title 10",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria11s.jpg",
-      alt: "Description for Image 11",
-      title: "Title 11",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria12.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria12s.jpg",
-      alt: "Description for Image 12",
-      title: "Title 12",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria13.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria13s.jpg",
-      alt: "Description for Image 13",
-      title: "Title 13",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria14.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria14s.jpg",
-      alt: "Description for Image 14",
-      title: "Title 14",
-    },
-    {
-      itemImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria15.jpg",
-      thumbnailImageSrc:
-        "https://primefaces.org/cdn/primevue/images/galleria/galleria15s.jpg",
-      alt: "Description for Image 15",
-      title: "Title 15",
     },
   ]
 
